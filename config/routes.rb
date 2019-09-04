67ee150f3081ba697529cccfb5155d3cbb57ba16
Shafquat <shafquatarefeen@gmail.com>
2019-09-04 08:33:35 -0400

    Finally connected venues to event in show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  devise_for :users
-	resources :events, :tickets, :venues
+	devise_for :admin_users, ActiveAdmin::Devise.config
+	ActiveAdmin.routes(self)
+	devise_for :users
+		resources :events, :tickets, :venues
 	
+	resources :venues do
+		resources :events
+	end
 	root "events#index"
 end
