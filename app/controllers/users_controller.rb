048a0a3dac2c5da71cb76fddc79a42fdff538198
Shafquat <shafquatarefeen@gmail.com>
2019-09-05 08:42:00 -0400

    Updated User profile page tp include tickets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 	def show
-	  @user = User.find(params[:id])
+		@user = User.find(params[:id])
+		@tickets = Ticket.where(user_id: @user.id).order("created_at DESC")
+	
 	end
+	
+
 end
