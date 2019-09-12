a3adee10d1fe8aee5420a63f9f6422d1597afa18
Shafquat <shafquatarefeen@gmail.com>
2019-09-11 23:02:27 -0400

    Prevented users from putting up same sec/row multiple times per game

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,7 @@
 class Ticket < ApplicationRecord
 	validates :section, :row, :price, :event_id, :user_id, :presence => true
+	validates :user_id, uniqueness: { :scope => [:section, :row, :event_id],
+    message: "You can't post the same tickets twice. Please delete your old listing first."  }
 	belongs_to :user
 	belongs_to :event
 end
